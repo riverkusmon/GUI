60326d7167f3857c5a599a45b1e4448fa562aba3
Andrew <andrewgardner@live.com>
2018-11-27 12:44:03 -0700

    added button and made it print to console when you press it

--- src/view/GUI/panel.java
+++ src/view/GUI/panel.java
@@ -2,10 +2,17 @@ package view.GUI;
 
 import javax.swing.*;
 import controller.GUI.controllerGUI;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
 
 public class panel extends JPanel
 {
 	private controllerGUI appController;
+	private JLabel textLabel;
+	private JButton colorButton;
+	
 	
 	public panel(controllerGUI appController)
 	{
@@ -13,7 +20,36 @@ public class panel extends JPanel
 		
 		this.appController = appController;
 		
+		this.textLabel = new JLabel("send minions");
+		this.colorButton = new JButton("click here for new minion");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
 	
+	private void setupPanel()
+	{
+		this.add(textLabel);
+		this.add(colorButton);
+	}
 	
+	private void setupLayout()
+	{
+		
+		
+	}
+	
+	private void  setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent mouseClick)
+				{
+					System.out.println("You made me minion");
+				}
+			});
+		
+		
+	}
 }
